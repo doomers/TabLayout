ecc5c5ee436f606422cedf14befef33f6011eff0
rahul357 <rahul40800@gmail.com>
2016-11-04 01:52:51 +0530

    Created TabLayout

---
+++ app/src/main/java/com/doomers/tablayout/Tab2.java
@@ -0,0 +1,23 @@
+package com.doomers.tablayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by doomers on 14/8/16.
+ */
+
+public class Tab2 extends Fragment {
+
+    //Overriden method onCreateView
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return inflater.inflate(R.layout.tab2, container, false);
+    }
+}
